3e7fa9ba040c22ac6af1b910518cf803b4229bca
tteokbokki-hater <pires11233@gmail.com>
2021-03-20 16:37:02 +0900

    계산기 이것저것

---
+++ CodeExplained/Calculator/calculator.js
@@ -0,0 +1,110 @@
+let calculator_buttons = [
+    {
+        name : "delete",
+        symbol : "⌫",
+        formula : false,
+        type : "key"
+    },{
+        name : "clear",
+        symbol : "C",
+        formula : false,
+        type : "key"
+    },{
+        name : "percent",
+        symbol : "%",
+        formula : "/100",
+        type : "number"
+    },{
+        name : "division",
+        symbol : "÷",
+        formula : "/",
+        type : "operator"
+    },{
+        name : "7",
+        symbol : 7,
+        formula : 7,
+        type : "number"
+    },{
+        name : "8",
+        symbol : 8,
+        formula : 8,
+        type : "number"
+    },{
+        name : "9",
+        symbol : 9,
+        formula : 9,
+        type : "number"
+    },{
+        name : "multiplication",
+        symbol : "×",
+        formula : "*",
+        type : "operator"
+    },{
+        name : "4",
+        symbol : 4,
+        formula : 4,
+        type : "number"
+    },{
+        name : "5",
+        symbol : 5,
+        formula : 5,
+        type : "number"
+    },{
+        name : "6",
+        symbol : 6,
+        formula : 6,
+        type : "number"
+    },{
+        name : "addition",
+        symbol : "+",
+        formula : "+",
+        type : "operator"
+    },,{
+        name : "1",
+        symbol : 1,
+        formula : 1,
+        type : "number"
+    },{
+        name : "2",
+        symbol : 2,
+        formula : 2,
+        type : "number"
+    },{
+        name : "3",
+        symbol : 3,
+        formula : 3,
+        type : "number"
+    },{
+        name : "subtraction",
+        symbol : "–",
+        formula : "-",
+        type : "operator"
+    },{
+        name : "0",
+        symbol : 0,
+        formula : 0,
+        type : "number"
+    },{
+        name : "comma",
+        symbol : ".",
+        formula : ".",
+        type : "number"
+    },{
+        name : "calculate",
+        symbol : "=",
+        formula : "=",
+        type : "calculate"
+    }
+];
+
+const output_result_element = document.querySelector('.result .value');
+const output_operation_element = document.querySelector('.operation .value');
+const input_element = document.querySelector('.input');
+
+function createButtons(){
+    const btn_per_row = 4;
+    let added_btn = 0;
+
+    if(added_btn % btn_per_row == 0){
+    }
+}
